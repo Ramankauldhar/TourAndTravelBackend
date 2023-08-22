8c39a9705ea3d58ef2c152527ef5f22641042fed
Raman kauldhar <rmnkaul979697@gmail.com>
2023-08-22 13:38:03 -0400

    verifyTokenFuc again fixed

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,48 +1,50 @@
 import jwt from 'jsonwebtoken';
- 
-export const verifyToken = (req, res, next) =>{
+
+export const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken;
-    
-    if(!token){
+
+    if (!token) {
         return res.status(401).json({
             success: false,
             message: "You are not authorized. Please Login First."
         });
     }
+
     jwt.verify(token, process.env.JWT_SECRET_KEY, (error, user) => {
-        if(error){
+        if (error) {
             return res.status(401).json({
                 success: false,
                 message: "Token is invalid"
-            })
+            });
         }
+
         req.user = user;
-        next(); //for next operation
-    })
-}
+        next(); // Continue to the next middleware/route handler
+    });
+};
 
 export const verifyUser = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        if(req.user.id === req.params.id || req.user.role === "admin"){
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.role === "user") {
             next();
-        }else{
+        } else {
             return res.status(401).json({
-                success:false,
-                message:"Please Login First."
+                success: false,
+                message: "Please Login First."
             });
         }
     });
-}
+};
 
 export const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        if(req.user.role === "admin"){
+    verifyToken(req, res, () => {
+        if (req.user.role === "admin") {
             next();
-        }else{
+        } else {
             return res.status(401).json({
-                success:false,
-                message:"Please Login First."
+                success: false,
+                message: "Please Login First."
             });
         }
     });
-}
+};
